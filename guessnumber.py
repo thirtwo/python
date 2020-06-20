affd264d0e27758d69129ca7e89cd0139485e76a
thirtwo <65511956+thirtwo@users.noreply.github.com>
2020-06-20 21:39:00 +0300

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -7,6 +7,7 @@ print("Well," , name , "i am thinking of a number between 1 and 20.", sep=" ")
 print("take a guess")
 number = random.randint(1,21)
 count = 0
+isFoundNumber = 0
 try:
     while count < 5:
         userNumber = input()
@@ -16,9 +17,11 @@ try:
             print("too low")
         else:
             print("gotcha","you got in",str(count),"turn", sep=" ")
+            isFoundNumber += 1
             break
         count += 1
-    print("sorry", "you wouldnt get in", str(count)+"turn", sep = " ")        
+    if isFoundNumber < 1 :
+        print("sorry", "you wouldnt get in", str(count)+"turn", sep = " ")       
         
 except:
     print("Error Occured")
